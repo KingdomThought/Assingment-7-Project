298847877d744f31e1e4529a69e8ca7da830fc5c
KingdomThought <79799753+KingdomThought@users.noreply.github.com>
2021-10-31 07:45:58 -0400

    Word Count Project
    This is a Simple program that counts how many "A" characters are in a string.

---
+++ src/WordCountTest.java
@@ -0,0 +1,20 @@
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordCountTest {
+
+	@Test
+	void test() {
+		Main test = new Main();
+		int result = test.countletter("America");
+		assertEquals(2, result);
+		
+	}
+
+	
+	}
+
